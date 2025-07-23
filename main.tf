c5396c53e6173860d23afcbfd5b53de83848c30e
rajwrdhn <rajwrdhn@gmail.com>
2025-07-23 20:37:11 +0200

    mongo atlas automated deployment

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "mongodbatlas" {
+  public_key  = var.public_key
+  private_key = var.private_key
+}
+
+resource "mongodbatlas_project" "cluster" {
+  name   = var.project_name
+  org_id = var.org_id
+}
+
+resource "mongodbatlas_cluster" "example" {
+  project_id = mongodbatlas_project.cluster.id
+  name       = var.cluster_name
+  provider_name = "AWS"
+  num_shards    = 0
+  replication_specs {
+    num_shards = 0
+    regions_config {
+      region_name     = "EU_CENTRAL_1"
+      priority        = 0
+      electable_nodes = 0
+      read_only_nodes = 0
+      analytics_nodes = 0
+    }
+  }
+  provider_instance_size_name = "M0"
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "public_key" {}
+variable "private_key" {}
+variable "org_id" {}
+variable "project_name" {}
+variable "cluster_name" {}
